7d8639e6558c864d3a4f1bd2639495d7a8546489
Toms-B <toms.bergis06@gmail.com>
2023-11-03 14:38:58 +0200

    pabeidzu 2.uzd

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,21 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
+kas ir RSS?
+
 """
+import feedparser
+
+# URL uz RSS plūsmu 
+
+rss_url='https://lv.wikipedia.org/wiki/RSS_protokols'
+
+# iegūsim RSS plūsmas datus
+kkk=feedparser.parse(rss_url)
+
+# parāda un jānoformē 
+
+for entry in kkk.entries:
+    print("Virsraksts: ", entry.title)
+    print("Saite: ", entry.link)
+    print('\n')
